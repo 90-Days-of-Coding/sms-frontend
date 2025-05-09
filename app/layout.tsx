d44fb5b1bfbcc917cdd5689d1bc0e91fd7f6a0b7
G4EVA-dev <tanzeglenn@gmail.com>
2025-05-09 05:03:54 +0100

    feat: Initial project setup with Next.js, TypeScript, Tailwind, ShadCN/UI, Zustand, React Query, and Socket.io

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Student Management System",
+  description:
+    "The Student Management System is a web-based platform designed to enhance the academic experience for university and college students. It provides a centralized hub for managing various aspects of student life",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
